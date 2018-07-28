ca9cb975bf9803091f3c347405d684dea18212ef
Eric Soderberg <eric.soderberg@hpe.com>
2018-07-27 17:11:38 -0700

    Initial gh-pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-const element = document.getElementById('content');
-ReactDOM.render(<App />, element);
-
-document.body.classList.remove('loading');
