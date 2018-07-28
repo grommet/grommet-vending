efa802b087ffc3d94d4b4cc880ddc3e71b5a4478
Eric Soderberg <eric.soderberg@hpe.com>
2018-07-28 10:04:55 -0700

    Work around a react issue with nested contexts

--- src/App.js
+++ src/App.js
@@ -13,36 +13,40 @@ import Login from './screens/Login';
 import Service from './screens/Service';
 import Services from './screens/Services';
 
+const Content = () => (
+  <ResponsiveContext.Consumer>
+    {responsive => (
+      <Grid
+        columns={responsive === 'wide'
+          ? ['flex', 'large', 'flex']
+          : ['flex']}
+        rows={['flex']}
+        areas={responsive === 'wide'
+          ? [{ name: 'main', start: [1, 0], end: [1, 0] }]
+          : [{ name: 'main', start: [0, 0], end: [0, 0] }]}
+      >
+        <Box gridArea="main">
+          <Route path="/" component={Header} />
+          <Box pad={responsive === 'narrow' && { horizontal: 'large' }}>
+            <Route exact path="/" component={Services} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/add" component={Add} />
+            <Route exact path="/service/edit/:id" component={Edit} />
+            <Route exact path="/service/location/:id" component={Location} />
+            <Route exact path="/service/:id" component={Service} />
+          </Box>
+        </Box>
+      </Grid>
+    )}
+  </ResponsiveContext.Consumer>
+);
+
 export default () => (
   <Router basename={process.env.PUBLIC_URL}>
-    <Server>
-      <Grommet>
-        <ResponsiveContext.Consumer>
-          {responsive => (
-            <Grid
-              columns={responsive === 'wide'
-                ? ['flex', 'large', 'flex']
-                : ['flex']}
-              rows={['flex']}
-              areas={responsive === 'wide'
-                ? [{ name: 'main', start: [1, 0], end: [1, 0] }]
-                : [{ name: 'main', start: [0, 0], end: [0, 0] }]}
-            >
-              <Box gridArea="main">
-                <Route path="/" component={Header} />
-                <Box pad={responsive === 'narrow' && { horizontal: 'large' }}>
-                  <Route exact path="/" component={Services} />
-                  <Route exact path="/login" component={Login} />
-                  <Route exact path="/add" component={Add} />
-                  <Route exact path="/service/edit/:id" component={Edit} />
-                  <Route exact path="/service/location/:id" component={Location} />
-                  <Route exact path="/service/:id" component={Service} />
-                </Box>
-              </Box>
-            </Grid>
-          )}
-        </ResponsiveContext.Consumer>
-      </Grommet>
-    </Server>
+    <Grommet>
+      <Server>
+        <Content />
+      </Server>
+    </Grommet>
   </Router>
 );
